7443e78d45b51b1c94d91c9a30465efb4d691d4f
fantigny <frederic.antigny@gmail.com>
2015-05-21 17:00:03 +0100

    cache impl

--- anfoya-tools/src/main/java/net/anfoya/java/cache/FileSerieSerializedMap.java
+++ anfoya-tools/src/main/java/net/anfoya/java/cache/FileSerieSerializedMap.java
@@ -21,7 +21,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class FileSerieSerializedMap<K extends Serializable, V extends Serializable> implements Map<K, V>{
 	private static final Logger LOGGER = LoggerFactory.getLogger(FileSerieSerializedMap.class);
-	private static final String TEMP = "/Users/fantigny/Desktop/tmp/"; //System.getProperty("java.io.tmpdir");
+	private static final String TEMP = System.getProperty("java.io.tmpdir");
 	private static final String FILE_NAME_PATTERN = TEMP + "fsm-cache-%s.ser";
 
 	private final int threshold;
