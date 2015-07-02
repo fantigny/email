7218e3b8b0e5d17813b8c9c0304fe0952838b912
fantigny <fantigny@PC-Guest>
2015-07-02 15:48:05 +0100

    undecorated stage

--- mail-browser/src/test/java/net/anfoya/mail/browser/javafx/entrypoint/CssTest.java
+++ mail-browser/src/test/java/net/anfoya/mail/browser/javafx/entrypoint/CssTest.java
@@ -1,8 +1,17 @@
 package net.anfoya.mail.browser.javafx.entrypoint;
 
+
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -16,16 +25,43 @@ public class CssTest extends Application {
 
 	@Override
 	public void start(final Stage primaryStage) throws Exception {
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		final BorderPane mainPane = new BorderPane();
+		final Scene scene = new Scene(mainPane, 800, 600);
+
+		final Button minButton = new Button("_");
+		minButton.setOnAction(e -> primaryStage.setIconified(true));
+		
+		final Button maxButton = new Button("+");
+		maxButton.setOnAction(e -> primaryStage.setMaximized(!primaryStage.isMaximized()));
+		primaryStage.maximizedProperty().addListener((ov, o, n) -> maxButton.setText(n? "-": "+"));
+		
+		final Button closeButton = new Button("x");
+		closeButton.setOnAction(e -> primaryStage.close());
+
+		final ImageView icon = new ImageView(new Image(getClass().getResourceAsStream("/net/anfoya/mail/image/Mail.png")));
+		icon.setFitHeight(16);
+		icon.setFitWidth(icon.getFitHeight());
+		
+		final Label title = new Label("title");
+		title.setGraphic(icon);
+		
+		final HBox filler = new HBox();
+		HBox.setHgrow(filler, Priority.ALWAYS);
+		
+		final HBox sysTools = new HBox(title, filler, minButton, maxButton, closeButton);
+		sysTools.setAlignment(Pos.CENTER);
+		mainPane.setTop(sysTools);
+		
 		final WebView view = new WebView();
 
 		final SplitPane splitPane = new SplitPane();
 		splitPane.getItems().add(view);
+		mainPane.setCenter(splitPane);
 
 //		splitPane.setOpacity(0);
 
-		final Scene scene = new Scene(splitPane, 800, 600);
 
-		primaryStage.initStyle(StageStyle.UNIFIED);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
