61a1f580fd5799067ae105f8af886976f5a5114b
fantigny <frederic.antigny@gmail.com>
2015-07-22 19:06:37 +0100

    connection progress test

--- mail-service-impl/src/test/java/net/anfoya/mail/gmail/javafx/ConnectionProgressTestApp.java
+++ mail-service-impl/src/test/java/net/anfoya/mail/gmail/javafx/ConnectionProgressTestApp.java
@@ -1,6 +1,10 @@
 package net.anfoya.mail.gmail.javafx;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
 public class ConnectionProgressTestApp extends Application {
@@ -9,9 +13,17 @@ public class ConnectionProgressTestApp extends Application {
 		launch(args);
 	}
 
+	int i = 0;
+
 	@Override
 	public void start(final Stage primaryStage) throws Exception {
 		final ConnectionProgress cp = new ConnectionProgress();
-		cp.setValue(.5, "test progress");
+		new Timer(true).schedule(new TimerTask() {
+			@Override
+			public void run() {
+				if (++i > 100) i = 0;
+				Platform.runLater(() -> cp.setValue(i / 100d, "test progress " + i + "%"));
+			}
+		}, 0, 200);
 	}
 }
