078e5dfdc7d439b96128f4d4ca8b66df2bc35a21
fantigny <frederic.antigny@gmail.com>
2015-07-24 15:30:45 +0100

    alphabetical sorting of selected tags with system tags first

--- anfoya-tag/src/main/java/net/anfoya/tag/javafx/scene/section/SelectedTagsPane.java
+++ anfoya-tag/src/main/java/net/anfoya/tag/javafx/scene/section/SelectedTagsPane.java
@@ -3,6 +3,7 @@ package net.anfoya.tag.javafx.scene.section;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
@@ -23,18 +24,30 @@ public class SelectedTagsPane<T extends Tag> extends FlowPane {
 
 	public void refresh(final Set<T> tags) {
 		final List<Button> buttons = new ArrayList<Button>();
-		for(final T tag: tags) {
-			final Button button = new Button(tag.getName() + CROSS);
-			button.getStyleClass().add("tag-label");
-			button.setOnAction(event -> {
-				if (clearTagCallBack != null) {
-					SelectedTagsPane.this.getChildren().remove(button);
-					clearTagCallBack.call(tag);
-				}
-			});
-			buttons.add(button);
+		final Set<T> sortedTags = new TreeSet<T>(tags);
+		for(final T tag: sortedTags) {
+			if (tag.isSystem()) {
+				buttons.add(createButton(tag));
+			}
+		}
+		for(final T tag: sortedTags) {
+			if (!tag.isSystem()) {
+				buttons.add(createButton(tag));
+			}
 		}
 		getChildren().setAll(buttons);
+	}
+
+	private Button createButton(final T tag) {
+		final Button button = new Button(tag.getName() + CROSS);
+		button.getStyleClass().add("tag-label");
+		button.setOnAction(event -> {
+			if (clearTagCallBack != null) {
+				SelectedTagsPane.this.getChildren().remove(button);
+				clearTagCallBack.call(tag);
+			}
+		});
+		return button;
 	}
 
 	public void setClearTagCallBack(final Callback<T, Void> callback) {
