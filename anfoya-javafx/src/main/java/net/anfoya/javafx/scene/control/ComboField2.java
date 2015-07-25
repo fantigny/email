c73273dc7875374df3640183ca73e7ea612200e8
fantigny <frederic.antigny@gmail.com>
2015-07-25 13:41:22 +0100

    combo field impl

--- anfoya-javafx/src/main/java/net/anfoya/javafx/scene/control/ComboField2.java
+++ anfoya-javafx/src/main/java/net/anfoya/javafx/scene/control/ComboField2.java
@@ -10,6 +10,7 @@ import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Point2D;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -24,114 +25,118 @@ import org.slf4j.LoggerFactory;
 
 public class ComboField2 extends TextField {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ComboField2.class);
-	
+
 	private final Popup popup;
-	private final ListView<String> listView;
 	private final ObservableList<String> items;
 	private final BooleanProperty showingProperty;
 
-	private volatile boolean backspaceEventReady;
+	private volatile boolean emptyBackspaceReady;
 	private Predicate<String> userFilter;
 
 	private EventHandler<ActionEvent> backspaceHandler;
-	
+
+	private final ListView<String> listView;
+
 	public ComboField2() {
 		super("");
 		this.items = FXCollections.observableArrayList();
-		
+
 		listView = new ListView<String>();
 		popup = new Popup();
+		popup.setAutoFix(true);
+		popup.setAutoHide(false);
 		popup.getContent().add(listView);
 		showingProperty = new SimpleBooleanProperty(false);
-		
+
 		userFilter = null;
-		backspaceEventReady = true;
-		
+		emptyBackspaceReady = true;
+
 		setOnKeyPressed(e -> {
-			if (KeyCode.BACK_SPACE == e.getCode() && backspaceEventReady) {
+			if (KeyCode.BACK_SPACE == e.getCode() && emptyBackspaceReady) {
 				backspaceHandler.handle(null);
 			}
-			backspaceEventReady = KeyCode.BACK_SPACE == e.getCode();
 		});
-		
+
 		textProperty().addListener((ov, o, n) -> {
-			LOGGER.debug("handle text changed from \"{}\" to \"{}\"", o, n);
-
-			if (n.isEmpty()) {
-				// text is empty
-				LOGGER.debug("text is empty", n);
-				showingProperty.set(false);
-				return;
-			}
-
-			final Task<ObservableList<String>> task = new Task<ObservableList<String>>() {
-				@Override
-				protected ObservableList<String> call() throws Exception {
-					return FXCollections.observableArrayList(ComboField2.this.items.filtered(s ->
-						userFilter == null
-							? StringHelper.containsIgnoreCase(s, n)
-							: userFilter.test(s)));
-				}
-			};
-			task.setOnSucceeded(e -> {
-				listView.setItems(task.getValue());
-				if (listView.getItems().isEmpty()) {
-					hide();
-					LOGGER.debug("no item match");
-				} else {
-					show();
-					LOGGER.debug("filtered {} item(s)", listView.getItems().size());
-				}
-			});
-			task.setOnFailed(e -> LOGGER.error("filtering items with \"{}\"", n, e.getSource().getException()));
-			ThreadPool.getInstance().submitHigh(task);
+			emptyBackspaceReady = n.isEmpty();
+			filter(n);
 		});
-		
+
 		focusedProperty().addListener((ov, o, n) -> {
 			if (o && !n && isShowing()) {
 				hide();
 			}
 		});
-		
+
 		showingProperty().addListener((ov, o, n) -> {
 			if (n) {
-				popup.show(this, ComboField2.this.getLayoutBounds().getMinX() + ComboField2.this.getLayoutX()
-						, ComboField2.this.getLayoutBounds().getMinY() + ComboField2.this.getLayoutY() + ComboField2.this.getHeight());
+				final Point2D p = listView.localToScene(0.0, 0.0);
+				popup.show(this,
+				        p.getX() + listView.getScene().getWindow().getX(),
+				        p.getY() + listView.getScene().getWindow().getY());
 			} else {
 				popup.hide();
 			}
 		});
 	}
 
-	public void setFilter(Predicate<String> userFilter) {
+	private void filter(final String n) {
+		LOGGER.debug("filter with text \"{}\"", n);
+		final Task<ObservableList<String>> task = new Task<ObservableList<String>>() {
+			@Override
+			protected ObservableList<String> call() throws Exception {
+				return FXCollections.observableArrayList(ComboField2.this.items.filtered(s ->
+					userFilter == null
+						? StringHelper.containsIgnoreCase(s, n)
+						: userFilter.test(s)));
+			}
+		};
+		task.setOnSucceeded(e -> {
+			listView.getItems().setAll(task.getValue());
+			if (listView.getItems().isEmpty()) {
+				hide();
+				LOGGER.debug("no item match");
+			} else {
+				show();
+				LOGGER.debug("filtered {} item(s)", listView.getItems().size());
+			}
+		});
+		task.setOnFailed(e -> LOGGER.error("filtering items with \"{}\"", n, e.getSource().getException()));
+		ThreadPool.getInstance().submitHigh(task);
+	}
+
+	public void setFilter(final Predicate<String> userFilter) {
 		this.userFilter = userFilter;
 	}
-	
-	public void setItems(Set<String> items) {
+
+	public void setItems(final Set<String> items) {
 		this.items.setAll(items);
+		if (isShowing()) {
+			filter(getText());
+		}
 	}
 
 	public boolean isShowing() {
 		return showingProperty.get();
 	}
-	
+
 	public BooleanProperty showingProperty() {
 		return showingProperty;
 	}
-	
+
 	public void show() {
 		showingProperty.set(true);
 	}
-	
+
 	public void hide() {
 		showingProperty.set(false);
 	}
 
-	public void setCellFactory(Callback<ListView<String>, ListCell<String>> factory) {
+	public void setCellFactory(final Callback<ListView<String>, ListCell<String>> factory) {
 		listView.setCellFactory(factory);
 	}
 
-	public void setOnBackspaceAction(EventHandler<ActionEvent> handler) {
+	public void setOnBackspaceAction(final EventHandler<ActionEvent> handler) {
 		backspaceHandler = handler;
 	}
 }
