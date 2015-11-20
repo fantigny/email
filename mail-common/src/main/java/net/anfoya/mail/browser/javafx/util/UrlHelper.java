dd0d32bd4ce58329077564eacf04b3964956b15b
fantigny <frederic.antigny@gmail.com>
2015-11-20 18:41:04 +0000

    fix url with parameters

--- mail-common/src/main/java/net/anfoya/mail/browser/javafx/util/UrlHelper.java
+++ mail-common/src/main/java/net/anfoya/mail/browser/javafx/util/UrlHelper.java
@@ -1,8 +1,10 @@
 package net.anfoya.mail.browser.javafx.util;
 
 import java.awt.Desktop;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,12 +19,13 @@ public class UrlHelper {
 		open(url, null);
 	}
 
-	public static void open(String url, Callback<String, Void> onMailto) {
+	public static void open(String urlStr, Callback<String, Void> onMailto) {
 		URI uri;
 		try {
-			uri = new URI(url);
-		} catch (final URISyntaxException e) {
-			LOGGER.error("reading address {}", url, e);
+			final URL url = new URL(urlStr);
+			uri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), null);
+		} catch (final URISyntaxException | MalformedURLException e) {
+			LOGGER.error("reading address {}", urlStr, e);
 			return;
 		}
 		final String scheme = uri.getScheme();
@@ -33,9 +36,9 @@ public class UrlHelper {
 				try {
 					Desktop.getDesktop().browse(uri);
 				} catch (final Exception e) {
-					LOGGER.error("handling link {}", url, e);
+					LOGGER.error("handling link {}", urlStr, e);
 				}
-			}, "handling url " + url);
+			}, "handling url " + urlStr);
 		}
 	}
 }
