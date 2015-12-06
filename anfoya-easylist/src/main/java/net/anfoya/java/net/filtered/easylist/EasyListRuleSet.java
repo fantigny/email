dd8ae5ac00ac349b8e0af11845d97cb48489efbb
fantigny <fantigny@MacBook-Fred>
2015-12-06 11:19:10 +0100

    ThreadPool impl update

--- anfoya-easylist/src/main/java/net/anfoya/java/net/filtered/easylist/EasyListRuleSet.java
+++ anfoya-easylist/src/main/java/net/anfoya/java/net/filtered/easylist/EasyListRuleSet.java
@@ -13,15 +13,15 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.anfoya.java.cache.LocalCache;
 import net.anfoya.java.io.SerializedFile;
 import net.anfoya.java.net.filtered.easylist.loader.InternetLoader;
 import net.anfoya.java.net.filtered.easylist.model.Rule;
 import net.anfoya.java.net.url.filter.RuleSet;
 import net.anfoya.java.util.concurrent.ThreadPool;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class EasyListRuleSet implements RuleSet {
 	private static final Logger LOGGER = LoggerFactory.getLogger(EasyListRuleSet.class);
@@ -130,7 +130,7 @@ public class EasyListRuleSet implements RuleSet {
 			exclusions.addAll(new SerializedFile<Set<Rule>>(CONFIG.getExclusionsFilePath()).load());
 			LOGGER.info("loaded {} local rules (in {}ms)", getRuleCount(), System.currentTimeMillis()-start);
 			return null;
-		});
+		}, "load local rules");
 		ThreadPool.getInstance().submitHigh(() -> {
 			try {
 				future.get();
@@ -143,7 +143,7 @@ public class EasyListRuleSet implements RuleSet {
 					|| exclusions.isEmpty()) {
 				loadInternet();
 			}
-		});
+		}, "load cache");
 	}
 
 	private void loadCache() {
