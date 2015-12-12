96b1cabdd60ade2277cb969e19fa6c7cc732a7ac
fantigny <frederic.antigny@gmail.com>
2015-12-12 11:44:30 +0000

    Revert "primary window size"
    This reverts commit dc02168c8c82d66f7af1a2c3a69c7b3f127991cf.

--- mail-client/src/main/java/net/anfoya/mail/client/javafx/entrypoint/MailClient.java
+++ mail-client/src/main/java/net/anfoya/mail/client/javafx/entrypoint/MailClient.java
@@ -8,14 +8,12 @@ import org.slf4j.LoggerFactory;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
@@ -69,7 +67,7 @@ public class MailClient extends Application {
 		checkVersion();
 	}
 
-	private void confirmClose(final WindowEvent e) {
+	private void confirmClose(WindowEvent e) {
 		if (Settings.getSettings().confirmOnQuit().get()) {
 			final CheckBox checkBox = new CheckBox("don't show again");
 			checkBox.selectedProperty().addListener((ov, o, n) -> {
@@ -155,16 +153,18 @@ public class MailClient extends Application {
 		}
 		mailBrowser.setOnSignout(e -> signout());
 
+		initTitle(stage);
+
 		stage.titleProperty().unbind();
+		stage.setWidth(1400);
+		stage.setHeight(800);
 		stage.setScene(mailBrowser);
 		stage.show();
 
-		initTitle(stage);
-		initSize(stage);
 		mailBrowser.initData();
 	}
 
-	private void initTitle(final Stage stage) {
+	private void initTitle(Stage stage) {
 		final Task<String> titleTask = new Task<String>() {
 			@Override
 			protected String call() throws Exception {
@@ -179,30 +179,6 @@ public class MailClient extends Application {
 		titleTask.setOnSucceeded(e -> stage.setTitle("FisherMail - " + e.getSource().getValue()));
 		titleTask.setOnFailed(e -> LOGGER.error("loading user's name", e.getSource().getException()));
 		ThreadPool.getInstance().submitLow(titleTask, "loading user's name");
-	}
-
-	private void initSize(final Stage stage) {
-		final Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
-
-		stage.setMinWidth(800);
-		stage.setMinHeight(600);
-
-		stage.setWidth(1400);
-		stage.setHeight(800);
-		if (stage.getWidth() > bounds.getWidth()
-				|| stage.getHeight() > bounds.getHeight()) {
-			stage.setWidth(bounds.getWidth());
-			stage.setHeight(bounds.getHeight());
-			stage.setMaximized(true);
-		}
-
-		if (stage.getX() < bounds.getMinX() || stage.getX() > bounds.getMaxX()) {
-			stage.setX(bounds.getMinX());
-		}
-		if (stage.getY() < bounds.getMinY() || stage.getY() > bounds.getMaxY()) {
-			stage.setY(bounds.getMinY());
-		}
-
 	}
 
 	private void initMacOs() {
@@ -271,10 +247,8 @@ public class MailClient extends Application {
 					final String message;
 					try {
 						final Message m = gmail.getMessage(t.getLastMessageId());
-						message = "from "
-								+ String.join(", ", MessageHelper.getNames(m.getMimeMessage().getFrom()))
-								+ "\r\n"
-								+ m.getSnippet();
+						message = "from " + String.join(", ", MessageHelper.getNames(m.getMimeMessage().getFrom()))
+								+ "\r\n" + m.getSnippet();
 					} catch (final Exception e) {
 						LOGGER.error("notifying new message for thread {}", t.getId(), e);
 						return;
