d4b82b4e8bf1c3dd072add610b10d6965a676901
pg02194 <pg02194@VDI-PG02194.rci.uk>
2018-02-18 18:27:14 +0000

    add start/ stop

--- mail-common/src/main/java/net/anfoya/mail/browser/javafx/settings/VersionHelper.java
+++ mail-common/src/main/java/net/anfoya/mail/browser/javafx/settings/VersionHelper.java
@@ -21,26 +21,26 @@ public class VersionHelper {
 	private static final Logger LOGGER = LoggerFactory.getLogger(VersionHelper.class);
 	private static final long REFRESH_DELAY = 1000 * 60 * 60 * 6;
 
-	private final ReadOnlyBooleanWrapper isLatestProperty;
+	private final ReadOnlyBooleanWrapper isLatest;
 
+	private final Timer timer;
+	
 	private String myVersion;
 	private String latestVersion;
 
 	public VersionHelper() {
 		myVersion = "";
 		latestVersion = "";
-		isLatestProperty = new ReadOnlyBooleanWrapper(true);
-
-		new Timer("version-checker", true).scheduleAtFixedRate(new TimerTask() {
-			@Override
-			public void run() {
-				refresh();
-				LOGGER.info("current ({}) == latest ({}) is {}"
-						, myVersion
-						, latestVersion
-						, isLatestProperty.get());
-			}
-		}, 0, REFRESH_DELAY);
+		isLatest = new ReadOnlyBooleanWrapper(true);
+		timer = new Timer("version-checker", true);
+	}
+	
+	public void start() {
+		timer.scheduleAtFixedRate(new TimerTask() { @Override public void run() { refresh(); } }, 0, REFRESH_DELAY);
+	}
+	
+	public void stop() {
+		timer.cancel();
 	}
 
 	public String getMyVersion() {
@@ -51,8 +51,8 @@ public class VersionHelper {
 		return latestVersion;
 	}
 
-	public ReadOnlyBooleanProperty isLastestProperty() {
-		return isLatestProperty.getReadOnlyProperty();
+	public ReadOnlyBooleanProperty isLastest() {
+		return isLatest.getReadOnlyProperty();
 	}
 
 	public void refresh() {
@@ -64,13 +64,15 @@ public class VersionHelper {
 				LOGGER.error("error read version from {}", VERSION_TXT_RESOURCE, e);
 			}
 		}
-		try (InputStream in = new URL(VERSION_TXT_URL).openStream();
+		try (InputStream in = new URL(VERSION_TXT_URL).openConnection().getInputStream();
 				BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
 			reader.lines().forEach(l -> latestVersion = l);
 		} catch (final IOException e) {
 			latestVersion = myVersion;
 			LOGGER.error("error read version from {}", VERSION_TXT_URL, e);
 		}
-		isLatestProperty.set(myVersion.equals(latestVersion));
+		isLatest.set(myVersion.equals(latestVersion));
+
+		LOGGER.info("current ({}) == latest ({}) is {}", myVersion, latestVersion, isLatest.get());
 	}
 }
