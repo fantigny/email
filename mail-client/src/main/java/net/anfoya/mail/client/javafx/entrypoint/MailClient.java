86e69a42835668d0f43048efb376b69dde0d8394
Fred A. <frederic.antigny@gmail.com>
2019-12-19 17:15:27 +0000

    define app user model id to allow the app to be pinned on win taskbar

--- mail-client/src/main/java/net/anfoya/mail/client/javafx/entrypoint/MailClient.java
+++ mail-client/src/main/java/net/anfoya/mail/client/javafx/entrypoint/MailClient.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sun.javafx.PlatformUtil;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
@@ -24,6 +26,7 @@ import net.anfoya.java.util.concurrent.ObservableExecutors;
 import net.anfoya.java.util.concurrent.ThreadPool;
 import net.anfoya.java.util.concurrent.ThreadPool.PoolPriority;
 import net.anfoya.javafx.notification.NotificationService;
+import net.anfoya.javafx.util.WinShell32;
 import net.anfoya.mail.browser.javafx.MailBrowser;
 import net.anfoya.mail.browser.javafx.MailBrowser.Mode;
 import net.anfoya.mail.browser.javafx.settings.Settings;
@@ -54,6 +57,11 @@ public class MailClient extends Application {
 
 
 	public static void main(final String[] args) {
+		if (PlatformUtil.isWindows()) {
+			WinShell32.setExplicitAppUserModelId(App.MAIL_CLIENT);
+			LOGGER.info("app user model id is set to {}", WinShell32.getCurrentProcessExplicitAppUserModelID());
+		}
+
 		Arrays.stream(OPTIONS).forEach(o -> System.setProperty(o[0], o[1]));
 		System.getProperties().forEach((k, v) -> LOGGER.info(OPTION_LOG, k, v));
 
