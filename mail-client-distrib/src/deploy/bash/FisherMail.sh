28c53a86a8f315c323b048601d721ce6f739b6c9
Fred A. <frederic.antigny@gmail.com>
2019-12-21 03:46:59 +0000

    start in background

--- mail-client-distrib/src/deploy/bash/FisherMail.sh
+++ mail-client-distrib/src/deploy/bash/FisherMail.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-$basedir/jre/bin/java -Djdk.gtk.version=2.2 -jar $basedir/FisherMail.jar
+$basedir/jre/bin/java -Djdk.gtk.version=2.2 -jar $basedir/FisherMail.jar </dev/null &>/dev/null &
