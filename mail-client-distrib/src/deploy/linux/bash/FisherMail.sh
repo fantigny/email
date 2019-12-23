d3bc7e80fac75e4cb7460282fc0603dce3ad4db8
Fred A. <frederic.antigny@gmail.com>
2019-12-23 12:38:27 +0000

    add java arg -client

--- mail-client-distrib/src/deploy/linux/bash/FisherMail.sh
+++ mail-client-distrib/src/deploy/linux/bash/FisherMail.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-$basedir/jre/bin/java -Djdk.gtk.version=2.2 -jar $basedir/FisherMail.jar </dev/null &>/dev/null &
+$basedir/jre/bin/java \
+	-client \
+	-Djdk.gtk.version=2.2 \
+	 -jar $basedir/FisherMail.jar \
+	 </dev/null &>/dev/null &
