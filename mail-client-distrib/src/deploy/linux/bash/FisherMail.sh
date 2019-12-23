3676b2e3db8669a2e9b18a7d37aa2f7f20f3de79
Fred A. <frederic.antigny@gmail.com>
2019-12-23 12:05:08 +0000

    add shortcut for rpm and fix fedora-32 looking for server jre

--- mail-client-distrib/src/deploy/linux/bash/FisherMail.sh
+++ mail-client-distrib/src/deploy/linux/bash/FisherMail.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/opt/FisherMail/FisherMail.sh
+basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+$basedir/jre/bin/java -Djdk.gtk.version=2.2 -jar $basedir/FisherMail.jar </dev/null &>/dev/null &
