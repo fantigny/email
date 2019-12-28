85245b53db937ffc36a236f446a84b0869627151
Fred A. <frederic.antigny@gmail.com>
2019-12-28 17:58:09 +0000

    add new service providers

---
+++ mail-service-outlook/src/main/java/net/anfoya/mail/outlook/OutlookServiceInfo.java
@@ -0,0 +1,25 @@
+package net.anfoya.mail.outlook;
+
+import net.anfoya.mail.model.Contact;
+import net.anfoya.mail.model.Message;
+import net.anfoya.mail.model.Section;
+import net.anfoya.mail.model.SimpleInfo;
+import net.anfoya.mail.model.Tag;
+import net.anfoya.mail.model.Thread;
+import net.anfoya.mail.service.MailService;
+import net.anfoya.mail.service.MailServiceInfo;
+
+@SuppressWarnings("serial")
+public class OutlookServiceInfo extends SimpleInfo implements MailServiceInfo {
+	private static final String NAME = "Outlook";
+	private static final String ICON_PATH = "/net/anfoya/mail/outlook/javafx/outlook.png";
+
+	public OutlookServiceInfo() {
+		super(NAME, ICON_PATH);
+	}
+
+	@Override
+	public MailService<? extends Section, ? extends Tag, ? extends Thread, ? extends Message, ? extends Contact> getMailService(String appName) {
+		return null;
+	}
+}
