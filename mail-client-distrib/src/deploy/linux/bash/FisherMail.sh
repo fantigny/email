3ea0e52b70ca2a40512525c3fa7ee728f208e6ad
Fred A. <frederic.antigny@gmail.com>
2019-12-28 09:07:23 +0000

    clean up

--- mail-client-distrib/src/deploy/linux/bash/FisherMail.sh
+++ mail-client-distrib/src/deploy/linux/bash/FisherMail.sh
@@ -2,9 +2,10 @@
 echo FisherMail is starting...
 execPath="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 $execPath/jre/bin/java \
-	-client \
 	-Djdk.gtk.version=2.2 \
-	-jar $execPath/FisherMail.jar \
+	-client \
+	-jar
+	$execPath/FisherMail.jar \
 		0</dev/null \
 		1>/dev/null \
 		2>/dev/null \
