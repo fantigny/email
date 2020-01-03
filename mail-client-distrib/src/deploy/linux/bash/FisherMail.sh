8841f8ae2dafd1a3f2a895d70327698281ea4798
Fred A. <frederic.antigny@gmail.com>
2020-01-03 20:18:55 +0000

    use openJdk and openJfx for linux distrib
    Former-commit-id: 94f30d3f8c3c17ffe6474669715d1ae5b4a657f6

--- mail-client-distrib/src/deploy/linux/bash/FisherMail.sh
+++ mail-client-distrib/src/deploy/linux/bash/FisherMail.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 echo FisherMail is starting...
 execPath="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-$execPath/jre/bin/java \
+$execPath/openjdk/bin/java \
+	--module-path $execPath/openjfx/lib \
+	--add-modules=javafx.swing,javafx.web \
+	--add-exports=javafx.web/com.sun.webkit.network=ALL-UNNAMED \
+	--add-exports=javafx.base/com.sun.javafx=ALL-UNNAMED \
 	-Djdk.gtk.version=2.2 \
 	-client \
 	-jar \
