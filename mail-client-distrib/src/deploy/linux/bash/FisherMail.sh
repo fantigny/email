19a4c772f0be1b9d497decb3b6bbd0c31ee804a2
Fred A. <frederic.antigny@gmail.com>
2020-01-04 20:25:06 +0000

    rename openjdk to jre

--- mail-client-distrib/src/deploy/linux/bash/FisherMail.sh
+++ mail-client-distrib/src/deploy/linux/bash/FisherMail.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 echo FisherMail is starting...
 execPath="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-$execPath/openjdk/bin/java \
-	--module-path $execPath/openjfx/lib \
-	--add-modules=javafx.swing,javafx.web \
-	--add-exports=javafx.web/com.sun.webkit.network=ALL-UNNAMED \
-	--add-exports=javafx.base/com.sun.javafx=ALL-UNNAMED \
-	-Djdk.gtk.version=2.2 \
+$execPath/jre/bin/java \
 	-client \
 	-jar \
 	$execPath/FisherMail.jar \
