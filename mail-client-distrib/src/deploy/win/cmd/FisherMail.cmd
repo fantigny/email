de3b846eae3786946efc84d277b048e516cf7e45
Fred A. <frederic.antigny@gmail.com>
2020-01-08 19:17:08 +0000

    win64 portable

---
+++ mail-client-distrib/src/deploy/win/cmd/FisherMail.cmd
@@ -0,0 +1,5 @@
+@start jre-11.0.5\bin\javaw ^
+--add-modules=javafx.web,javafx.swing ^
+--add-exports=javafx.web/com.sun.webkit.network=ALL-UNNAMED ^
+--add-exports=javafx.base/com.sun.javafx=ALL-UNNAMED ^
+-jar FisherMail.jar
